0790f3fd11ac0baa9250d1fec479fdd9102e2094
brianrego03 <brian.rego03@gmail.com>
2025-04-08 11:31:52 +0530

    Create basic server config in index js file

---
+++ index.js
@@ -0,0 +1,25 @@
+const {createServer} = require("node:http");
+const fs=require("node:fs");
+
+const portName=8080;
+const server=createServer((req,res)=>{
+    if(req.url==="/"){
+        fs.readFile('index.html','utf-8',(err,data)=>{
+            if(err){
+                res.writeHead(500,{"content-type":"text/plain"});
+                res.end("Server error")
+            }
+            else {
+                res.writeHead(200,{"content-type":"text/html"});
+                res.end(data);
+            }
+        });
+    }
+    else if(req.url==="about"){
+        fs.readFile('about.html','utf-8');
+    }
+
+})
+server.listen(portName,()=>{
+    console.log(`Server running at localhost:${portName}`);
+})
